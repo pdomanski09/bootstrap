e917f03ce9da14e1f58b9c3a3c4e46357043deb5
Piotr Domanski <piotrdomanski@tlen.pl>
2018-05-13 16:47:06 +0200

    end of project

--- warsztacik/js/main.js
+++ warsztacik/js/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+$(function(){
+	//smooth scroll
+	//takim zapisem wykluczamy wszystkie href które zawierają tylko hashtag
+	$("a[href*='#']:not([href='#'])").click(function(e){
+		$("html, body").animate({ scrollTop: $(this.hash).offset().top}, 500)
+	});
+	
+	// scroll nav color change
+	
+	$(window).scroll(function(){
+		if($(window).scrollTop() > 50) {
+		$("#main-nav").addClass("scroll");
+		} else {
+			$("#main-nav").remoweClass("scroll");
+		}
+	});
+});
